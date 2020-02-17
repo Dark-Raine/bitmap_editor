5289b86a3613ad5af127920c09585e02b01ee171
Anderson Coelho <axzonexgamer@gmail.com>
2020-02-17 21:10:55 +0000

    added integer coercion to the restrict function since the parameters come as strings. changed the create bitmap command as to be able to dynamically be called. Lastly added comments to the function

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,18 +1,16 @@
-require 'pry'
 class Bitmap
     # this will execute changes to the bit map from a parsed command
     attr_reader :map
 
 
     def restrict(num)
-        num.clamp(1,250)
+        num.to_i.clamp(1,250)
     end
 
-    def create_map(x:,y:)
+    def i(x:,y:)
+        # the create bitmap command
         @map = Array.new(restrict(y)) {Array.new(restrict(x), 0)}
     end
     
     
 end
-
-binding.pry
