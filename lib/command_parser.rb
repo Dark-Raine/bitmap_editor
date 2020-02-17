1032b6edf9e70c5e97ee9fea0507a13fcac5d330
Anderson Coelho <axzonexgamer@gmail.com>
2020-02-17 10:58:20 +0000

    this class will parse commands and ensure they are valid. Finally returning an object that the bit map class can consume to take action

---
+++ lib/command_parser.rb
@@ -0,0 +1,8 @@
+# parse string commands into valid command objects for the bitmap editor
+
+class CommandParser
+    
+    def execute(unparsed_command)
+        # this will parse the command if it's valid
+    end
+end
