974e846235a740380561a3d43b1ca97d3b300a65
Anderson Coelho <axzonexgamer@gmail.com>
2020-02-17 13:58:27 +0000

    the bitmap method that will actually create the bitmap

--- lib/bitmap.rb
+++ lib/bitmap.rb
@@ -1,14 +1,18 @@
-
+require 'pry'
 class Bitmap
     # this will execute changes to the bit map from a parsed command
     attr_reader :map
-    def initialize(x, y)
-        @map = Array.new(restrict(y)) {Array.new(restrict(x), 0)} 
-    end
+
 
     def restrict(num)
         num.clamp(1,250)
     end
+
+    def create_map(x,y)
+        @map = Array.new(restrict(y)) {Array.new(restrict(x), 0)}
+    end
     
     
 end
+
+binding.pry
