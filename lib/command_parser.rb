9914a962f4ae3d201330832da8cf5be6e21b926d
Anderson Coelho <axzonexgamer@gmail.com>
2020-02-17 18:29:45 +0000

    removed space_filter func and opted to do the filtering in the function for now

--- lib/command_parser.rb
+++ lib/command_parser.rb
@@ -1,22 +1,27 @@
 # parse string commands into valid command objects for the bitmap editor
-require 'pry'
+# require 'pry'
 class CommandParser
     
-    def execute(unparsed_command)
-        # this will parse the command if it's valid
-    end
+    # def execute(unparsed_command)
+    #     # this will parse the command if it's valid
+    #     parsers = CommandParser.instance_methods(false)
+    #     parsers.select {|parser| !!self.send(parser, unparsed_command)}
+    # end
 
     def parse_create_bitmap_command(command)
         validator = /(^[I])(\s)(\d{1,3})(\s)(\d{1,3}$)/
         validated = validator.match(command)
-        return false unless validated
-            parsed = space_filter(validated.captures)
-            {command: parsed[0], x: parsed[1], y: parsed[2]}
+        return false unless if validated
+        to_filter = validated.captures
+        parsed = to_filter.filter {|element| element != " "}
+        {command: parsed[0], x: parsed[1], y: parsed[2]}
     end
-
-    def space_filter(arr)
-        arr.filter {|element| element != " "}
-    end
+    
+    # def space_filter(arr)
+    #     binding.pry
+    #     # arr.split(" ")
+    #     arr.filter {|element| element != " "}
+    # end
 end
 
-binding.pry
+# CommandParser.new.execute("I 5 6")
