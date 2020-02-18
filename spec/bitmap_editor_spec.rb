e05065e1dcd32b73d798aefaf43d4c1499f8b176
Anderson Coelho <axzonexgamer@gmail.com>
2020-02-18 21:58:14 +0000

    end to end test now creates its own text file with commands to test against. The user can now change the contents of show.txt in examples without this test breaking.

--- spec/bitmap_editor_spec.rb
+++ spec/bitmap_editor_spec.rb
@@ -4,11 +4,18 @@ require 'json'
 
 describe 'BitmapEditor' do
     before(:all) do
-        @file = "examples/show.txt"
+        @file = "test.txt"
+        file = File.open("test.txt", "w") do |f|
+            f.write("L 10 10 B\nI 5 6\nC\nV 3 3 3 X\nH 6 3 6 Y\nP\nS 0 0\nS")
+        end
         @bitmap_editor = BitmapEditor.new
         capture = StringIO.new
         $stdout = capture
     end
+    after(:all) do
+      File.delete("test.txt")
+    end
+    
 
     context 'Application' do
         it 'Should take in commands from text file and produce the correct bitmap' do
