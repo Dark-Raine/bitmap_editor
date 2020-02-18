efdbf3416f47202eabc669597ee80d739182ef3b
Anderson Coelho <axzonexgamer@gmail.com>
2020-02-18 17:21:04 +0000

    added parser function for horizontal command. rectified some comments throughout class.

--- lib/command_parser.rb
+++ lib/command_parser.rb
@@ -1,13 +1,5 @@
 # parse string commands into valid command objects for the bitmap editor
-# require 'pry'
 class CommandParser
-    
-    # def execute(unparsed_command)
-    #     # this will parse the command if it's valid
-    #     parsers = CommandParser.instance_methods(false)
-    #     parsers.select {|parser| !!self.send(parser, unparsed_command)}
-    # end
-
     def parse_create_bitmap_command(command)
         #function for parsing the I command
         # validates with regex and returns an object 
@@ -20,7 +12,7 @@ class CommandParser
     end
     
     def parse_no_parameter_command(command)
-        #function for parsing the C command
+        #function for parsing the C/S command
         # validates with regex and returns an object 
         validator = /(^[C]$|^[S]$)/
         validated = validator.match(command)
@@ -41,7 +33,7 @@ class CommandParser
     end
 
     def parse_colour_vertical_segment_command(command)
-        #function for parsing the L command
+        #function for parsing the V command
         # validates with regex and returns an object 
         validator = /(^[V])(\s)(\d{1,3})(\s)(\d{1,3})(\s)(\d{1,3})(\s)([A-Z]{1}$)/
         validated = validator.match(command)
@@ -50,11 +42,15 @@ class CommandParser
         parsed = to_filter.filter {|element| element != " "}
         {command: parsed[0], parameters:{ x: parsed[1], y1: parsed[2], y2: parsed[3] ,colour: parsed[4]}}
     end
-    # def space_filter(arr)
-    #     binding.pry
-    #     # arr.split(" ")
-    #     arr.filter {|element| element != " "}
-    # end
-end
 
-# CommandParser.new.execute("I 5 6")
+    def parse_colour_horizontal_segment_command(command)
+        #function for parsing the H command
+        # validates with regex and returns an object 
+        validator = /(^[H])(\s)(\d{1,3})(\s)(\d{1,3})(\s)(\d{1,3})(\s)([A-Z]{1}$)/
+        validated = validator.match(command)
+        return false unless validated
+        to_filter = validated.captures
+        parsed = to_filter.filter {|element| element != " "}
+        {command: parsed[0], parameters:{ x1: parsed[1], x2: parsed[2], y: parsed[3] ,colour: parsed[4]}}
+    end
+end
